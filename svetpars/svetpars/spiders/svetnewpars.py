4ae8503d7be48d226d5eb9f60aaa2b55b2e37aca
Natali280853 <dgorby2000@gmail.com>
2024-10-23 23:55:18 +0500

    add parse

--- svetpars/svetpars/spiders/svetnewpars.py
+++ svetpars/svetpars/spiders/svetnewpars.py
@@ -7,4 +7,10 @@ class SvetnewparsSpider(scrapy.Spider):
     start_urls = ["https://www.divan.ru/ekaterinburg/category/svet"]
 
     def parse(self, response):
-        pass
+        lamps = response.css("div._Ud0k")
+        for lamp in lamps:
+            yield {
+                'name': lamp.css('div.lsooF span::text').get(),
+                'price': lamp.css('div.pY3d2 span::text').get(),
+                'url': lamp.css('a').attrib['href']
+            }
